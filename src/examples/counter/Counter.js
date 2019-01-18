3ffb5efc534f0a53e592f1b0a10c40c8f1df8f35
Mike Smith <msmith@techempower.com>
2019-01-18 15:45:26 -0800

    Fixed some bugs and incorrects in readme (#2)

--- src/examples/counter/Counter.js
+++ src/examples/counter/Counter.js
@@ -14,8 +14,8 @@ export default function Counter() {
   return (
     <div>
       <input className="val" value={state.count} readOnly />
-      <button className="inc" onClick={actions.increment} />
-      <button className="dec" onClick={actions.decrement} />
+      <button className="inc" onClick={() => actions.increment()} />
+      <button className="dec" onClick={() => actions.decrement()} />
       <button className="add5" onClick={() => actions.add(5)} />
       <button className="set" onClick={() => actions.set(137)} />
       <button className="addSum" onClick={() => actions.addSum(3, 4)} />
